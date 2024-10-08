baace3ff32c5e3e65aff7addce8eb2f687b8d791
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2024-10-08 12:53:44 +0200

    chore: format code

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-class UserController extends AbstractController
-{
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
-    }
+class UserController extends AbstractController {
+
+  #[Route('/user', name: 'app_user')]
+  public function index(): JsonResponse {
+    return $this->json([
+      'message' => 'Welcome to your new controller!',
+      'path' => 'src/Controller/UserController.php',
+    ]);
+  }
+
 }
