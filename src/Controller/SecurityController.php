f56f7b2cf89a74d2f92be193f58b8f36a5dff7a0
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2024-10-09 22:52:31 +0200

    ops: add SecurityController

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/security', name: 'app_security')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SecurityController.php',
+        ]);
+    }
+}
