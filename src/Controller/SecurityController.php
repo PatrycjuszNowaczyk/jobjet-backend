b0808d8253948f821af2e117ead25a8f9c7bdeeb
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2024-10-09 23:10:44 +0200

    chore: clear SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,12 +8,5 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/security', name: 'app_security')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SecurityController.php',
-        ]);
-    }
+
 }
