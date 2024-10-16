48973887617bd13077522462cc4280ac53d27f7c
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2024-10-16 03:02:44 +0200

    fix: rename repository
    Signed-off-by: Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\UsersRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[ORM\Entity(repositoryClass: UsersRepository::class)]
+#[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-class Users implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
