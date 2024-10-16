8386e9c691017d832667701b3650a0d0d7815504
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2024-10-16 03:03:25 +0200

    fix: rename UserController
    Signed-off-by: Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/users')]
-class UsersController extends AbstractController
+class UserController extends AbstractController
 {
 
     /**
